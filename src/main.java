ba9a01db4f1e629a8df751b9317d6470b5dcd7e1
Sh-Revo <shandov@i.ua>
2019-09-30 19:19:38 +0300

    123

--- src/main.java
+++ src/main.java
@@ -15,8 +15,12 @@ public class main {
         points[5]=new second(5,-1) ;
         points[6]=new second(1,-3);
         points[7]=new second(-1,-1);
+        for (int j = 0; j < n; j++) {
+            System.out.println("Old points[" + (j+1) + "]: "+points[j].getX()+" "+points[j].getY());
+        }
+        System.out.println();
         for (int j = 0; j <n ; j++) {
-            System.out.println("New points[" + (j+1) + "]: "+points[j].getX()+" "+points[j].getY());
+            //System.out.println("New points[" + (j+1) + "]: "+points[j].getX()+" "+points[j].getY());
             if (multiplication[j] < 0) {
                 if (j==n-1){
                     points[j]=points[j-n+1];
@@ -26,26 +30,26 @@ public class main {
        /* for (int i = 0; i < 5; i++) {
             System.out.println(i+": "+ points[i].getX()+"   "+points[i].getY());
         }*/
-            System.out.println("Vectors: ");
+        //    System.out.println("Vectors: ");
         for ( i = 0; i <= n-1; i++) {
             if (i==n-1){
                 vectors[i]=new second(points[i].getX()-points[i-n+1].getX(),points[i].getY()-points[i-n+1].getY());
-                System.out.println(vectors[i].getX()+" "+vectors[i].getY());
+             //   System.out.println(vectors[i].getX()+" "+vectors[i].getY());
             } else {
             vectors[i]=new second(points[i+1].getX()-points[i].getX(),points[i+1].getY()-points[i].getY());
-            System.out.println(vectors[i].getX()+" "+vectors[i].getY());
+           // System.out.println(vectors[i].getX()+" "+vectors[i].getY());
             }
         }
 
-            System.out.println("Proiz: ");
+       //     System.out.println("Proiz: ");
       //  vectors[0].setX(points[1].x-points[0].x);
         for ( i = 0; i <= n-1; i++) {
             if (i == n-1) {
                 multiplication[i] = vectors[i].getX() * vectors[i-n+1].getX() + vectors[i].getY() * vectors[i - n+1].getY();
-                System.out.println(multiplication[i]);
+            //    System.out.println(multiplication[i]);
             } else {
                 multiplication[i] = vectors[i + 1].getX() * vectors[i].getX() + vectors[i].getY() * vectors[i + 1].getY();
-                System.out.println(multiplication[i]);
+            //    System.out.println(multiplication[i]);
             }
         }
 
